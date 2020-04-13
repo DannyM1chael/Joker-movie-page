c77953126306f4e615bc710b7ca5ca48455ac09a
DannyM1chael <coldsoulrusirk@gmail.com>
2020-04-13 15:23:57 +0300

    add js

--- script.js
+++ script.js
@@ -1,4 +1,13 @@
-let controlBtn = document.querySelector('.play');
-controlBtn.addEventListener('click', function toogle() {
-    console.log('click')
+let playBtn = document.querySelector('.play');
+    playBtn.addEventListener('click', function playTrailer() {
+    let trailer = document.querySelector('.trailer');
+    trailer.style.visibility = 'visible';
+    trailer.style.opacity = 1;
+})
+
+let closeBtn = document.querySelector('.close');
+    closeBtn.addEventListener('click', function closeTrailer() {
+    let trailer = document.querySelector('.trailer');
+    trailer.style.visibility = 'hidden';
+    trailer.style.opacity = 0;
 })
